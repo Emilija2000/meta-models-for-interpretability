b19735997f6bfb53e5059bdcf6f51155e6e65283
Emilija2000 <emilidjordjevic@gmail.com>
2023-07-15 21:22:34 +0100

    tokenizing with layer type indicators

--- chunking/preprocessing.py
+++ chunking/preprocessing.py
@@ -1,9 +1,9 @@
-import dataclasses
-import haiku as hk
+import functools
 import jax
+from jax import flatten_util
 import jax.numpy as jnp
 from jax.typing import ArrayLike
-from typing import Dict, Sequence, Tuple, Callable
+from typing import Dict, Tuple, Callable
 import chex
 import numpy as np
 
@@ -73,7 +73,7 @@ def preprocess(
         ) -> Tuple[jax.Array, Callable]:
     """Preprocess a pytree of parameters into a flat array of chunks."""
     params, unfilter = filter_layers(params)
-    flat_params, unflatten = jax.flatten_util.ravel_pytree(params)
+    flat_params, unflatten = flatten_util.ravel_pytree(params)
     padded = pad_to_chunk_size(flat_params, chunk_size)
     chunks = padded.reshape(-1, chunk_size)
     
@@ -92,7 +92,7 @@ def get_unpreprocess(
     """Preprocess once to get the unpreprocess function."""
     params, _ = filter_layers(params)
     chunks, unpreprocess = preprocess(params, chunk_size)
-    raveled_params = jax.flatten_util.ravel_pytree(params)[0]
+    raveled_params = flatten_util.ravel_pytree(params)[0]
     if verbose:
         print()
         print(f"Number of (relevant) layers per net: {len(params)}")
@@ -115,20 +115,12 @@ def preprocess_layerwise(
     
     # Get original shapes
     param_shapes = jax.tree_util.tree_map(lambda x: x.shape, params)
-    
-    # First, reshape all the layers into a single vector per layer
-    #flat_params = {k: jnp.concatenate([v.flatten() for v in layer.values()])
-    #        for k, layer in params.items()} 
-    #original_lengths = {k: v.size for k, v in flat_params.items()}
-    # chunk each layer separately
-    #flat_params = {k: chunk_array(v, chunk_size) for k, v in flat_params.items()}
-    
     flat_params = jax.tree_util.tree_map(lambda x: x.flatten(), params)
     original_lengths = jax.tree_util.tree_map(lambda x: len(x), flat_params)
     flat_params = jax.tree_util.tree_map(lambda x: chunk_array(x, chunk_size), flat_params)
     
     # flatten to array of chunks
-    flat_params, unflatten = jax.flatten_util.ravel_pytree(flat_params)
+    flat_params, unflatten = flatten_util.ravel_pytree(flat_params)
     chunked = flat_params.reshape(-1, chunk_size)
     
     def unprocess(chunks: ArrayLike) -> Dict[str, Dict[str, ArrayLike]]:
@@ -150,7 +142,7 @@ def get_unpreprocess_layerwise(
     params, _ = filter_layers(params)
     chunks, unpreprocess = preprocess_layerwise(params, chunk_size)
     param_shapes = get_param_shapes(params)
-    raveled_params = jax.flatten_util.ravel_pytree(params)[0]
+    raveled_params = flatten_util.ravel_pytree(params)[0]
     if verbose:
         print()
         print(f"Number of (relevant) layers per net: {len(params)}")
@@ -160,13 +152,99 @@ def get_unpreprocess_layerwise(
         print(f"Number of chunks per net: {chunks.shape[0]}")
         print()
     return unpreprocess
+
+########################
+# Added indicators
+def preprocess_withindicators(
+        params: Dict[str, Dict[str, ArrayLike]], 
+        chunk_size: int
+        ) -> Tuple[jax.Array, Callable]:
+    """Preprocess a pytree of parameters into a flat array of chunks,
+    where each layer is chunked separately, with indicators for layer type
+    and with indicators for end-of-layer."""
     
+    # Define the layer types
+    layer_types = {"conv": 0, "linear": 1, "other": 2}
+    num_layer_types = len(layer_types)
+    def get_layer_type(layer) -> int:
+        for type_name in layer_types:
+            if type_name in layer.key.lower():
+                return layer_types[type_name]
+        return layer_types["other"]
+    
+    # filter out some layer types
+    params, unfilter = filter_layers(params)
+    
+    # Get original shapes and flatten parameters
+    param_shapes = jax.tree_util.tree_map(lambda x: x.shape, params)
+    flat_params = jax.tree_util.tree_map(lambda x: x.flatten(), params)
+    original_lengths = jax.tree_util.tree_map(lambda x: len(x), flat_params)
+
+    # Chunk params
+    chunked_params = jax.tree_util.tree_map(lambda x: chunk_array(x, chunk_size), flat_params)
+    
+    def add_embeddings(path, chunk):
+        layer = path[0]
+        layer_type_idx = get_layer_type(layer)
+        layer_type_one_hot = [0] * num_layer_types
+        layer_type_one_hot[layer_type_idx] = 1
+
+        # Prepare embeddings
+        embeddings = jnp.tile(jnp.array(layer_type_one_hot), (len(chunk), 1))
+
+        # Prepare end of layer token
+        end_token = np.zeros((len(chunk), 1))
+        if len(chunk) > 0:
+            end_token[-1, 0] = 1
+        end_token = jnp.array(end_token)
+
+        # Combine chunks, embeddings, and end token
+        combined = jnp.hstack([chunk, embeddings, end_token])
+
+        return combined
+    
+    embedded_params = jax.tree_util.tree_map_with_path(add_embeddings, chunked_params)
+
+    # Flatten to array of chunks
+    flat_params, unflatten = flatten_util.ravel_pytree(embedded_params)
+    chunked = flat_params.reshape(-1, chunk_size + num_layer_types + 1)  
+    
+    def unprocess(chunks: ArrayLike) -> Dict[str, Dict[str, ArrayLike]]:
+        """Inverse of preprocess."""
+        orig_len = chunks.shape[1] - num_layer_types - 1
+        chunks = chunks[:, :orig_len]
+        params = unflatten(chunks.flatten()[:len(flat_params)])
+        params = jax.tree_util.tree_map(lambda x, orig_len: x.flatten()[:orig_len], params, original_lengths)
+        params = jax.tree_util.tree_map(lambda x, shape: x.reshape(shape), params, param_shapes)
+        return unfilter(params)
+    
+    return chunked, unprocess
+
+def get_unpreprocess_withindicators(
+        params: Dict[str, Dict[str, ArrayLike]],
+        chunk_size: int,
+        verbose: bool = True,
+        ) -> Callable:
+    """Preprocess once to get the unpreprocess function."""
+    params, _ = filter_layers(params)
+    chunks, unpreprocess = preprocess_withindicators(params, chunk_size)
+    param_shapes = get_param_shapes(params)
+    raveled_params = flatten_util.ravel_pytree(params)[0]
+    if verbose:
+        print()
+        print(f"Number of (relevant) layers per net: {len(params)}")
+        print(f"Number of parameters per net: "
+            f"{raveled_params.shape[0]}")
+        print(f"Chunk size: {chunk_size}")
+        print(f"Number of chunks per net: {chunks.shape[0]}")
+        print()
+    return unpreprocess
 
 #######################
 # Check for high variance or mean of params
 
 def flatten(x):
-    return jax.flatten_util.ravel_pytree(x)[0]
+    return flatten_util.ravel_pytree(x)[0]
 
 def is_fine(params: dict):
     """Return false if std or mean is too high."""
@@ -190,5 +268,16 @@ def filter_data(*arrays):
     print(f"Filtered out {num_filtered} nets.")
     return arrays_filtered
 
+##################
+# Fast versions without unpreprocess
 
+@functools.partial(jax.jit,static_argnums=(1))
+def fast_process_layerwise(params: Dict[str, Dict[str, ArrayLike]], chunk_size: int) -> jax.Array:
+    flat_params = jax.tree_util.tree_map(lambda x: x.flatten(), params)
+    flat_params = jax.tree_util.tree_map(lambda x: chunk_array(x, chunk_size), flat_params)
+    flat_params, _ = flatten_util.ravel_pytree(flat_params)
+    chunked = flat_params.reshape(-1, chunk_size)
+    return chunked
 
+def batch_process_layerwise(param_list, chunk_size):
+    return jnp.stack(list(map(functools.partial(fast_process_layerwise, chunk_size=chunk_size), param_list)))
